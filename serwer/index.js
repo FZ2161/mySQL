c4ce7715a53f0901b20174ca1205cc383c2c5b8f
FZ2161 <filipzalewski44@gmail.com>
2023-03-06 09:17:54 +0100

    first endpoint

--- serwer/index.js
+++ serwer/index.js
@@ -1,1 +1,18 @@
-drtyj
+const cors = require('cors')
+const express = require('express')
+const mysql = require('mysql')
+
+const app = express()
+const port = 3000
+
+app.use(cors())
+
+app.get('/', (req, resss)=>{
+
+    resss.send('Ok')
+
+})
+
+app.listen(port, ()=>{
+    console.log(`port: ${port}`)
+})
